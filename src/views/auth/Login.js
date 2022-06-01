ce6abc2f33e984334badd2d7d025b2133e6ac056
ledanhtrong <90688469+ledanhtrong@users.noreply.github.com>
2022-06-01 07:08:22 +0700

    config firebase, refactor login page

--- src/views/auth/Login.js
+++ src/views/auth/Login.js
@@ -13,6 +13,8 @@ import uiConfig from "firebase-config/firebase-ui";
 import { auth } from "firebase-config/firebase";
 import useAuthActions from "hooks/useAuthActions";
 
+// firebase
+
 const useStyles = makeStyles(componentStyles);
 
 function Login() {
