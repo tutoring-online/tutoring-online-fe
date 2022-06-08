795e14ff2dad4514c24f2f7015da73f8b94f8c11
linh128 <61704840+linh128@users.noreply.github.com>
2022-06-08 22:06:57 +0700

    update admin page
    remove the right card
    and update the profile form

--- src/views/admin/Profile.js
+++ src/views/admin/Profile.js
@@ -18,14 +18,11 @@ import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 // @mui/icons-material components
-import LocationOn from "@mui/icons-material/LocationOn";
-import School from "@mui/icons-material/School";
 
 // core components
 import UserHeader from "components/Headers/UserHeader.js";
 
 import componentStyles from "assets/theme/views/admin/profile.js";
-import boxShadows from "assets/theme/box-shadow.js";
 
 const useStyles = makeStyles(componentStyles);
 
@@ -46,7 +43,7 @@ function Profile() {
           <Grid
             item
             xs={12}
-            xl={8}
+            xl={12}
             component={Box}
             marginBottom="3rem"
             classes={{ root: classes.gridItemRoot + " " + classes.order2 }}
@@ -100,49 +97,59 @@ function Profile() {
                 </Box>
                 <div className={classes.plLg4}>
                   <Grid container>
-                    <Grid item xs={12} lg={6}>
-                      <FormGroup>
-                        <FormLabel>Email</FormLabel>
-                        <FormControl
-                          variant="filled"
-                          component={Box}
-                          width="100%"
-                          marginBottom="1rem!important"
-                        >
-                          <Box
-                            paddingLeft="0.75rem"
-                            paddingRight="0.75rem"
-                            component={FilledInput}
-                            autoComplete="off"
-                            type="email"
-                            defaultValue="linhse111111@fu.vn"
-                            disabled="true"
-                          />
-                        </FormControl>
-                      </FormGroup>
+                    <Grid container item xs={12} lg={8}>
+                      <Grid item xs={12} lg={9}>
+                        <FormGroup>
+                          <FormLabel>Email</FormLabel>
+                          <FormControl
+                            variant="filled"
+                            component={Box}
+                            width="100%"
+                            marginBottom="1rem!important"
+                          >
+                            <Box
+                              paddingLeft="0.75rem"
+                              paddingRight="0.75rem"
+                              component={FilledInput}
+                              autoComplete="off"
+                              type="email"
+                              defaultValue="linhse111111@fu.vn"
+                              disabled="true"
+                            />
+                          </FormControl>
+                        </FormGroup>
+                      </Grid>
+                      <Grid item xs={12} lg={9}>
+                        <FormGroup>
+                          <FormLabel>Name</FormLabel>
+                          <FormControl
+                            variant="filled"
+                            component={Box}
+                            width="100%"
+                            marginBottom="1rem!important"
+                          >
+                            <Box
+                              paddingLeft="0.75rem"
+                              paddingRight="0.75rem"
+                              component={FilledInput}
+                              autoComplete="off"
+                              type="text"
+                              defaultValue="Linh"
+                            />
+                          </FormControl>
+                        </FormGroup>
+                      </Grid>
+                    </Grid>
+                    <Grid container item xs={6} lg={3}>
+                      <Grid>
+                        <Box width='250px'
+                             height='250px'
+                             component={Card}
+                             image/>
+                      </Grid>
                     </Grid>
                   </Grid>
                   <Grid container>
-                    <Grid item xs={12} lg={6}>
-                      <FormGroup>
-                        <FormLabel>Name</FormLabel>
-                        <FormControl
-                          variant="filled"
-                          component={Box}
-                          width="100%"
-                          marginBottom="1rem!important"
-                        >
-                          <Box
-                            paddingLeft="0.75rem"
-                            paddingRight="0.75rem"
-                            component={FilledInput}
-                            autoComplete="off"
-                            type="text"
-                            defaultValue="Linh"
-                          />
-                        </FormControl>
-                      </FormGroup>
-                    </Grid>
                     <Grid item xs={12} lg={6}>
                       <FormGroup>
                         <FormLabel>Gender</FormLabel>
@@ -154,13 +161,13 @@ function Profile() {
                         >
                           <RadioGroup row>
                             <FormControlLabel
-                              value="Male"
+                              value="male"
                               control={<Radio />}
                               label="Male"
-                              checked="true"
+                              defaultChecked="true"
                             />
                             <FormControlLabel
-                              value="Female"
+                              value="female"
                               control={<Radio />}
                               label="Female"
                             />
@@ -251,7 +258,7 @@ function Profile() {
                             component={FilledInput}
                             autoComplete="off"
                             type="text"
-                            defaultValue="Image/Axxx/Bxxx/Cxxx...."
+                            defaultValue="Image/Axxx"
                           />
                         </FormControl>
                       </FormGroup>
@@ -335,209 +342,6 @@ function Profile() {
                   </Grid>
                 </div>
               </CardContent>
-            </Card>
-          </Grid>
-          <Grid
-            item
-            xs={12}
-            xl={4}
-            component={Box}
-            marginBottom="3rem!important"
-            classes={{ root: classes.order1 + " " + classes.marginBottomXl0 }}
-          >
-            <Card classes={{ root: classes.cardRoot }}>
-              <Box component={Grid} container justifyContent="center">
-                <Grid item xs={12} lg={3}>
-                  <Box position="relative">
-                    <Box
-                      component="img"
-                      src={
-                        require("assets/img/theme/team-4-800x800.jpg").default
-                      }
-                      alt="..."
-                      maxWidth="180px"
-                      borderRadius="50%"
-                      position="absolute"
-                      left="50%"
-                      boxShadow={boxShadows.boxShadow + "!important"}
-                      className={classes.profileImage}
-                    />
-                  </Box>
-                </Grid>
-              </Box>
-              <Box
-                component={CardHeader}
-                border="0!important"
-                textAlign="center"
-                paddingBottom="0!important"
-                paddingTop="8rem!important"
-                classes={{ root: classes.cardHeaderRootProfile }}
-                subheader={
-                  <Box display="flex" justifyContent="space-between">
-                    <Button
-                      variant="contained"
-                      size="small"
-                      classes={{ root: classes.buttonRootInfo }}
-                    >
-                      Connect
-                    </Button>
-                    <Button
-                      variant="contained"
-                      size="small"
-                      classes={{ root: classes.buttonRootDark }}
-                    >
-                      Message
-                    </Button>
-                  </Box>
-                }
-              ></Box>
-              <Box
-                component={CardContent}
-                classes={{ root: classes.ptMd4 }}
-                paddingTop="0!important"
-              >
-                <Grid container>
-                  <Grid item xs={12}>
-                    <Box
-                      padding="1rem 0"
-                      justifyContent="center"
-                      display="flex"
-                      className={classes.mtMd5}
-                    >
-                      <Box
-                        textAlign="center"
-                        marginRight="1rem"
-                        padding=".875rem"
-                      >
-                        <Box
-                          component="span"
-                          fontSize="1.1rem"
-                          fontWeight="700"
-                          display="block"
-                          letterSpacing=".025em"
-                          className={classes.typographyRootH6}
-                        >
-                          22
-                        </Box>
-                        <Box
-                          component="span"
-                          fontSize=".875rem"
-                          color={theme.palette.gray[500]}
-                        >
-                          Friends
-                        </Box>
-                      </Box>
-                      <Box
-                        textAlign="center"
-                        marginRight="1rem"
-                        padding=".875rem"
-                      >
-                        <Box
-                          component="span"
-                          fontSize="1.1rem"
-                          fontWeight="700"
-                          display="block"
-                          letterSpacing=".025em"
-                          className={classes.typographyRootH6}
-                        >
-                          10
-                        </Box>
-                        <Box
-                          component="span"
-                          fontSize=".875rem"
-                          color={theme.palette.gray[500]}
-                        >
-                          Photos
-                        </Box>
-                      </Box>
-                      <Box textAlign="center" padding=".875rem">
-                        <Box
-                          component="span"
-                          fontSize="1.1rem"
-                          fontWeight="700"
-                          display="block"
-                          letterSpacing=".025em"
-                          className={classes.typographyRootH6}
-                        >
-                          89
-                        </Box>
-                        <Box
-                          component="span"
-                          fontSize=".875rem"
-                          color={theme.palette.gray[500]}
-                        ></Box>
-                      </Box>
-                    </Box>
-                  </Grid>
-                </Grid>
-                <Box textAlign="center">
-                  <Typography variant="h3">
-                    Jessica Jones
-                    <Box component="span" fontWeight="300">
-                      , 27
-                    </Box>
-                  </Typography>
-                  <Box
-                    component={Typography}
-                    variant="h5"
-                    fontWeight="300!important"
-                    display="flex"
-                    alignItems="center"
-                    justifyContent="center"
-                  >
-                    <Box
-                      component={LocationOn}
-                      width="1.25rem!important"
-                      height="1.25rem!important"
-                    ></Box>
-                    Bucharest, Romania
-                  </Box>
-                  <Box
-                    component={Typography}
-                    variant="h5"
-                    marginTop="3rem!important"
-                  >
-                    Solution Manager - Creative Tim Officer
-                  </Box>
-                  <Box
-                    display="flex"
-                    alignItems="center"
-                    justifyContent="center"
-                    fontSize="1rem"
-                  >
-                    <Box
-                      component={School}
-                      width="1.25rem!important"
-                      height="1.25rem!important"
-                      marginRight=".5rem"
-                    ></Box>
-                    University of Computer Science
-                  </Box>
-                  <Box
-                    component={Divider}
-                    marginTop="1.5rem!important"
-                    marginBottom="1.5rem!important"
-                  ></Box>
-                  <Box
-                    component="p"
-                    fontWeight="300"
-                    lineHeight="1.7"
-                    marginBottom="1rem"
-                    fontSize="1rem"
-                  >
-                    Ryan — the name taken by Melbourne-raised, Brooklyn-based
-                    Nick Murphy — writes, performs and records all of his own
-                    music.
-                  </Box>
-                  <a
-                    href="#mui"
-                    className={classes.cardProfileLink}
-                    onClick={(e) => e.preventDefault()}
-                  >
-                    Show More
-                  </a>
-                </Box>
-              </Box>
             </Card>
           </Grid>
         </Grid>
