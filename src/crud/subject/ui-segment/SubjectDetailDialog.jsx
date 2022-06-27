8b2290c5c6354ef0bcdfe9b9f087b7896586e7be
linh128 <61704840+linh128@users.noreply.github.com>
2022-06-27 20:47:49 +0700

    v
    Edit AdminDetailDialog
    Category
    Subject (temp)

--- src/crud/subject/ui-segment/SubjectDetailDialog.jsx
+++ src/crud/subject/ui-segment/SubjectDetailDialog.jsx
@@ -1,11 +1,5 @@
-
-import {
-    Button,
-    Dialog,
-    DialogContent,
-    Grid,
-} from "@mui/material";
-import EditIcon from '@mui/icons-material/Edit';
+import { Button, Dialog, DialogContent, Grid, Typography } from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
 
 import { useForm } from "react-hook-form";
 import React, { useEffect, useState } from "react";
@@ -24,192 +18,215 @@ import SelectField from "components/Form/SelectField";
 import useCategoryList from "hooks/category/useCategoryList";
 
 const schema = yup.object().shape({
-    code: yup.string()
-        .required("Code is required"),
-    name: yup.string()
-        .required("Name is required"),
-    categoryId: yup.string().nullable()
-        .required("Category is required"),
+  code: yup.string().required("Code is required"),
+  name: yup.string().required("Name is required"),
+  categoryId: yup.string().nullable().required("Category is required"),
 });
 
 const getDefaultValues = (subject) => {
-    if (!subject) return {
-        status: SUBJECT_STATUSES.ACTIVE
+  if (!subject)
+    return {
+      status: SUBJECT_STATUSES.ACTIVE,
     };
 
-    return {
-        ...subject,
-    }
-}
+  return {
+    ...subject,
+  };
+};
 
 export default function SubjectDetailDialog({
-    open,
-    onClose,
-    onSubmit,
-    subject,
-    mode,
-    title = "Tutor Detail",
-    submitButton = {
-        text: "Confirm"
-    }
+  open,
+  onClose,
+  onSubmit,
+  subject,
+  mode,
+  title = "Tutor Detail",
+  submitButton = {
+    text: "Confirm",
+  },
 }) {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+    control,
+  } = useForm({
+    mode: "onSubmit",
+    reValidateMode: "onBlur",
+    defaultValues: getDefaultValues(subject),
+    resolver: yupResolver(schema),
+  });
 
-    const { register, handleSubmit, reset, formState: { errors }, control } = useForm({
-        mode: "onSubmit",
-        reValidateMode: "onBlur",
-        defaultValues: getDefaultValues(subject),
-        resolver: yupResolver(schema)
-    })
+  const { categoryList } = useCategoryList();
 
-    const { categoryList } = useCategoryList();
+  const [isEditing, setIsEditing] = useState(false);
 
-    const [isEditing, setIsEditing] = useState(false);
+  useEffect(() => {
+    setIsEditing(() => {
+      if (mode === CRUD_MODE.create) return true;
+      if (mode === CRUD_MODE.edit) return true;
+      return false;
+    });
+  }, [mode]);
 
-    useEffect(() => {
-        setIsEditing(() => {
-            if (mode === CRUD_MODE.create) return true;
-            if (mode === CRUD_MODE.edit) return true;
-            return false;
-        })
-    }, [mode])
+  const isDisabled = !isEditing;
 
-    const isDisabled = !isEditing;
+  const preparedBeforeSubmit = (data) => {
+    const preparedData = {
+      ...data,
+    };
+    onSubmit && onSubmit(preparedData);
+  };
 
-    const preparedBeforeSubmit = (data) => {
-        const preparedData = {
-            ...data,
-        }
-        onSubmit && onSubmit(preparedData);
-    }
+  const enableEdit = () => {
+    setIsEditing(true);
+  };
 
-    const enableEdit = () => {
-        setIsEditing(true);
-    }
+  const cancelEdit = () => {
+    setIsEditing(false);
+    reset();
+  };
 
-    const cancelEdit = () => {
-        setIsEditing(false);
-        reset();
-    }
-
+  function renderDisplayContent() {
     return (
-        <Dialog
-            open={open}
-            onClose={onClose}
-            maxWidth="md"
-        >
-            <CustomDialogTitle
-                title={title}
-                onClose={onClose}
-            />
-            <DialogContent>
-                <form
-                    onSubmit={handleSubmit(preparedBeforeSubmit)}
-                >
-                    <Grid container>
-                        <Grid item xs={12} lg={6}>
-                            <TextField
-                                label="Code"
-                                required={true}
-                                inputProps={{
-                                    ...register("code")
-                                }}
-                                error={errors.code?.message}
-                                disabled={isDisabled}
-                            />
-                        </Grid>
-                        <Grid item xs={12} lg={6}>
-                            <TextField
-                                label="Name"
-                                required={true}
-                                inputProps={{
-                                    ...register("name")
-                                }}
-                                error={errors.name?.message}
-                                disabled={isDisabled}
-                            />
-                        </Grid>
-                        <Grid item xs={12} lg={6}>
-                            <SelectField
-                                label="Category"
-                                name="categoryId"
-                                control={control}
-                                required={true}
-                                options={categoryList.map(item => ({
-                                    label: item.name,
-                                    value: item.id
-                                }))}
-                                error={errors.categoryId?.message}
-                                disabled={isDisabled}
-                            />
-                        </Grid>
+      <div>
+        <Grid container paddingLeft="4rem">
+          <Grid item xs={12} lg={6} marginBottom="20px">
+            <Typography fontSize={18} fontWeight="bold">
+              Name:
+            </Typography>
+            <Typography>{subject?.name || "N/A"}</Typography>
+          </Grid>
+          <Grid item xs={12} lg={6} marginBottom="20px">
+            <Typography fontSize={18} fontWeight="bold">
+              Status:
+            </Typography>
+            <Typography>DKJGDL:GKJDLKGJSDLFGKJDFGLKJ</Typography>
+          </Grid>
+          <Grid item xs={12} lg={12} marginBottom="20px">
+            <Typography fontSize={18} fontWeight="bold">
+              Description:
+            </Typography>
+            <Typography>SDFHSKFJSDKLFJSLDKFJ</Typography>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
 
-                        <Grid item xs={12}>
-                            <TextField
-                                label="Description"
-                                inputProps={{
-                                    ...register("description"),
-                                    multiline: true,
-                                    rows: 4,
-                                    type: "date"
-                                }}
-                                error={errors.description?.message}
-                                disabled={isDisabled}
-                            />
-                        </Grid>
+  return (
+    <Dialog open={open} onClose={onClose} maxWidth="md">
+      <CustomDialogTitle title={title} onClose={onClose} />
+      <DialogContent>
+        {isEditing ? (
+          <form onSubmit={handleSubmit(preparedBeforeSubmit)}>
+            <Grid container>
+              <Grid item xs={12} lg={6}>
+                <TextField
+                  label="Code"
+                  required={true}
+                  inputProps={{
+                    ...register("code"),
+                  }}
+                  error={errors.code?.message}
+                  disabled={isDisabled}
+                />
+              </Grid>
+              <Grid item xs={12} lg={6}>
+                <TextField
+                  label="Name"
+                  required={true}
+                  inputProps={{
+                    ...register("name"),
+                  }}
+                  error={errors.name?.message}
+                  disabled={isDisabled}
+                />
+              </Grid>
+              <Grid item xs={12} lg={6}>
+                <SelectField
+                  label="Category"
+                  name="categoryId"
+                  control={control}
+                  required={true}
+                  options={categoryList.map((item) => ({
+                    label: item.name,
+                    value: item.id,
+                  }))}
+                  error={errors.categoryId?.message}
+                  disabled={isDisabled}
+                />
+              </Grid>
 
-                        <Grid item xs={12}>
-                            <RadioGroupField
-                                label="Status"
-                                name="status"
-                                row={true}
-                                options={LIST_SUBJECT_STATUS.map(item => ({
-                                    ...item,
-                                    label: renderSubjectStatus(item.value),
-                                }))}
-                                control={control}
-                                disabled={isDisabled}
-                            />
-                        </Grid>
-                    </Grid>
-                </form>
-            </DialogContent>
-            <CustomDialogActions>
-                {isEditing ?
-                    <>
-                        <Button
-                            variant=""
-                            color="info"
-                            size="medium"
-                            onClick={cancelEdit}
-                            sx={{ background: "#fff", "&:hover": { background: "#f3f3f3" } }}
-                        >
-                            Cancel
-                        </Button>
+              <Grid item xs={12}>
+                <TextField
+                  label="Description"
+                  inputProps={{
+                    ...register("description"),
+                    multiline: true,
+                    rows: 4,
+                    type: "date",
+                  }}
+                  error={errors.description?.message}
+                  disabled={isDisabled}
+                />
+              </Grid>
 
+              <Grid item xs={12}>
+                <RadioGroupField
+                  label="Status"
+                  name="status"
+                  row={true}
+                  options={LIST_SUBJECT_STATUS.map((item) => ({
+                    ...item,
+                    label: renderSubjectStatus(item.value),
+                  }))}
+                  control={control}
+                  disabled={isDisabled}
+                />
+              </Grid>
+            </Grid>
+          </form>
+        ) : (
+          renderDisplayContent()
+        )}
+      </DialogContent>
+      <CustomDialogActions>
+        {isEditing ? (
+          <>
+            <Button
+              variant=""
+              color="info"
+              size="medium"
+              onClick={cancelEdit}
+              sx={{ background: "#fff", "&:hover": { background: "#f3f3f3" } }}
+            >
+              Cancel
+            </Button>
 
-                        <Button
-                            variant="contained"
-                            color="primary"
-                            size="medium"
-                            type="submit"
-                            onClick={handleSubmit(preparedBeforeSubmit)}
-                        >
-                            {submitButton?.text}
-                        </Button>
-                    </>
-
-                    :
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        size="medium"
-                        onClick={enableEdit}
-                        startIcon={<EditIcon />}
-                    >
-                        Enable Edit
-                    </Button>
-                }
-            </CustomDialogActions>
-        </Dialog>
-    )
+            <Button
+              variant="contained"
+              color="primary"
+              size="medium"
+              type="submit"
+              onClick={handleSubmit(preparedBeforeSubmit)}
+            >
+              {submitButton?.text}
+            </Button>
+          </>
+        ) : (
+          <Button
+            variant="contained"
+            color="primary"
+            size="medium"
+            onClick={enableEdit}
+            startIcon={<EditIcon />}
+          >
+            Enable Edit
+          </Button>
+        )}
+      </CustomDialogActions>
+    </Dialog>
+  );
 }
