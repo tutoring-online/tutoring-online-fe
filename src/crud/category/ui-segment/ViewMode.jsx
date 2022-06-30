29002b44dfabe92894b9600bbb875021f296add2
linh128 <61704840+linh128@users.noreply.github.com>
2022-06-30 11:56:09 +0700

    noob description
    .

--- src/crud/category/ui-segment/ViewMode.jsx
+++ src/crud/category/ui-segment/ViewMode.jsx
@@ -72,22 +72,26 @@ const Header = ({ category }) => (
 
 const Description = ({ category }) => (
   <GroupBox>
-      <Grid container>
-          <Grid item xs={12}>
-              <FormLabel sx={{ fontSize: "18px" }}>
-                  Description
-              </FormLabel>
-          </Grid>
-
-          <Grid item xs={12}>
-              <DisplayField
-                  value={category?.address || <NoInformation
-                   />}                   
-              />
-          </Grid>
+    <Grid container>
+      <Grid item xs={12}>
+        <FormLabel sx={{ fontSize: "18px" }}>Description</FormLabel>
       </Grid>
+
+      <Grid item xs={12}>
+        <FormLabel sx={{ fontSize: "14px" }}>
+          This is description Lorem Ipsum has been the industry's standard dummy
+          text ever since the 1500s, when an unknown printer took a galley of
+          type and scrambled it to make a type specimen book. It has survived
+          not only five centuries, but also the leap into electronic
+          typesetting, remaining essentially unchanged. It was popularised in
+          the 1960s with the release of Letraset sheets containing Lorem Ipsum
+          passages, and more recently with desktop publishing software like
+          Aldus PageMaker including versions of Lorem Ipsum.
+        </FormLabel>
+      </Grid>
+    </Grid>
   </GroupBox>
-)
+);
 
 export default function ViewMode({ category }) {
   return (
