e9891fcbb164dcdd9581699fd8ffc2fa4d8df5db
linh128 <61704840+linh128@users.noreply.github.com>
2022-06-30 11:47:09 +0700

    category skeleton
    .

--- src/crud/category/ui-segment/ViewModeSkeleton.jsx
+++ src/crud/category/ui-segment/ViewModeSkeleton.jsx
@@ -1,64 +1,18 @@
-import {Grid, Box, Skeleton} from "@mui/material";
+import { Grid, Box, Skeleton } from "@mui/material";
 import LabelSkeleton from "components/Skeleton/LabelSkeleton";
 import ValueSkeleton from "components/Skeleton/ValueSkeleton";
 
 const ViewModeSkeleton = () => (
-    <Grid container>
-        <Grid item xs={12}>
-            <Box
-                display="flex"
-                marginBottom="1rem"
-                height="80px"
-            >
-                <Skeleton
-                    variant="circular"
-                    width={60}
-                    height={60}
-                    animation="wave"
-                />
-                <Box
-                    display="flex"
-                    flexDirection="column"
-                    marginLeft="1rem"
-                >
-                    <Skeleton
-                        variant="text"
-                        height={24}
-                        width={160}
-                        animation="wave"
-                    />
-                    <Skeleton
-                        variant="text"
-                        height={24}
-                        width={300}
-                        animation="wave"
-                        style={{ marginTop: "0.25rem" }}
-                    />
-                </Box>
-            </Box>
-        </Grid>
-        <Grid item xs={12} lg={6} marginBottom="1rem">
-            <LabelSkeleton />
-            <ValueSkeleton />
-        </Grid>
-        <Grid item xs={12} lg={6} marginBottom="1rem">
-            <LabelSkeleton />
-            <ValueSkeleton />
-        </Grid>
-        <Grid item xs={12} lg={6} marginBottom="1rem">
-            <LabelSkeleton />
-            <ValueSkeleton />
-        </Grid>
-        <Grid item xs={12} lg={6} marginBottom="1rem">
-            <LabelSkeleton />
-            <ValueSkeleton />
-
-        </Grid>
-        <Grid item xs={12} marginBottom="1rem">
-            <LabelSkeleton />
-            <ValueSkeleton />
-        </Grid>
+  <Grid container>
+    <Grid item xs={12} marginBottom="1rem">
+      <LabelSkeleton />
+      <ValueSkeleton />
     </Grid>
-)
+    <Grid item xs={12} marginBottom="1rem">
+      <LabelSkeleton />
+      <ValueSkeleton />
+    </Grid>
+  </Grid>
+);
 
 export default ViewModeSkeleton;
