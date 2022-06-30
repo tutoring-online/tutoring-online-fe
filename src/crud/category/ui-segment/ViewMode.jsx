02cb33f94c5da034e5c60884d27a032e9225adda
linh128 <61704840+linh128@users.noreply.github.com>
2022-06-30 11:38:03 +0700

    category basic viewmode
    .

--- src/crud/category/ui-segment/ViewMode.jsx
+++ src/crud/category/ui-segment/ViewMode.jsx
@@ -1,29 +1,105 @@
-import React from 'react';
-import { Box, Grid, Typography } from '@mui/material';
+import React from "react";
+import { FormLabel, Grid } from "@mui/material";
+import DisplayField from "components/Form/DisplayField";
+import GroupBox from "components/Form/GroupBox";
+import NoInformation from "components/Text/NoInformation";
+import { Box } from "@mui/material";
+import { validDate } from "helpers/dateUtils";
+import { getLocaleDateTimeString } from "helpers/dateUtils";
+import { getLocaleDateString } from "helpers/dateUtils";
+import { renderCategoryStatus } from "settings/category-setting";
+
+const getDisplayDateTime = (date) => {
+  return validDate(date) ? getLocaleDateTimeString(date) : "N/A";
+};
+
+const getDisplayDate = (date) => {
+  return validDate(date) ? getLocaleDateString(date) : "N/A";
+};
+
+const PublishDate = ({ createdDate }) => (
+  <Box fontSize="14px">
+    <Box display="flex">
+      <FormLabel sx={{ margin: 0 }}>Publish at</FormLabel>
+      <Box marginLeft="4px" fontSize="14px">
+        {getDisplayDate(createdDate)}
+      </Box>
+    </Box>
+  </Box>
+);
+
+const StatusBar = ({ status }) => (
+  <Box fontSize="13px" marginLeft="8px">
+    {renderCategoryStatus(status)}
+  </Box>
+);
+
+const UpdatedDate = ({ updatedDate }) => (
+  <Box marginLeft="auto" fontSize="14px">
+    <Box display="flex">
+      <FormLabel sx={{ margin: 0 }}>Last updated on</FormLabel>
+      <Box marginLeft="4px" fontSize="14px">
+        {getDisplayDateTime(updatedDate)}
+      </Box>
+    </Box>
+  </Box>
+);
+
+const Header = ({ category }) => (
+  <Box marginBottom="1rem" className="detail-header">
+    <Box
+      display="grid"
+      gridTemplateRows="1fr 1fr"
+      gridTemplateColumns="1fr"
+      flexGrow="1"
+      padding="0 1rem"
+      minHeight="80px"
+    >
+      <Box display="flex" alignItems="center" height="100%">
+        <Box fontWeight="600" fontSize="17px">
+          {category?.name}
+        </Box>
+        <StatusBar status={category?.status} />
+      </Box>
+
+      <Box display="flex" alignItems="center">
+        <PublishDate createdDate={category?.createdDate} />
+        <UpdatedDate updatedDate={category?.updatedDate} />
+      </Box>
+    </Box>
+  </Box>
+);
+
+const Description = ({ category }) => (
+  <GroupBox>
+      <Grid container>
+          <Grid item xs={12}>
+              <FormLabel sx={{ fontSize: "18px" }}>
+                  Description
+              </FormLabel>
+          </Grid>
+
+          <Grid item xs={12}>
+              <DisplayField
+                  value={category?.address || <NoInformation
+                   />}                   
+              />
+          </Grid>
+      </Grid>
+  </GroupBox>
+)
 
 export default function ViewMode({ category }) {
-    return (
-        <Box component="div">
-            <Grid container paddingLeft="4rem">
-                <Grid item xs={12} lg={6} marginBottom="20px">
-                    <Typography fontSize={18} fontWeight="bold">
-                        Name:
-                    </Typography>
-                    <Typography>{category?.name || "N/A"}</Typography>
-                </Grid>
-                <Grid item xs={12} lg={6} marginBottom="20px">
-                    <Typography fontSize={18} fontWeight="bold">
-                        Status:
-                    </Typography>
-                    <Typography>{category?.status || "N/A"}</Typography>
-                </Grid>
-                <Grid item xs={12} lg={12} marginBottom="20px">
-                    <Typography fontSize={18} fontWeight="bold">
-                        Description:
-                    </Typography>
-                    <Typography>{category?.description || "N/A"}</Typography>
-                </Grid>
-            </Grid>
-        </Box>
-    );
+  return (
+    <Box component="div">
+      <Grid container>
+        <Grid item xs={12}>
+          <Header category={category} />
+        </Grid>
+        <Grid item xs={12}>
+          <Description category={category} />
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
